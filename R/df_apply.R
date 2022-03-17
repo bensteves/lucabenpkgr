1f6e34446dfb53f7edf1e13c9b825cd8ffa8fd43
Ben Steves <bjs48@students.calvin.edu>
2022-03-17 01:51:52 -0400

    added df_apply function, edited parts of data file

--- R/df_apply.R
+++ R/df_apply.R
@@ -1,3 +1,10 @@
-df_apply <- function(.data, .f, .condition, .else, ...) {
-
+df_apply <- function(.data, .f, .condition = is.numeric, .else = identity, ...) {
+  .data |> lapply(
+    function(x) 
+      if (.condition(x)) 
+        .f(x, ...) 
+      else 
+        .else(x)
+  ) %>%
+    as_tibble()
 }
--- data-raw/mydata.r
+++ data-raw/mydata.r
@@ -1,3 +1,4 @@
+#Luca's data
 lego_sets <- read.csv("sets.csv")
 
 lego_themes <- read.csv("themes.csv")
@@ -5,22 +6,26 @@ lego_themes <- read.csv("themes.csv")
 lego <- merge(lego_themes, lego_sets, by.x ="id", by.y ="theme_id", all.x = TRUE, all.y = FALSE)
 
 lego_wrangle <- lego %>%
-  group_by(name.x, year) %>%
-  filter(grepl('Star Wars', name.x)) %>%
-  summarise(n=n())
+  dplyr::group_by(name.x, year) %>%
+  dplyr::filter(grepl('Star Wars', name.x)) %>%
+  dlpyr::summarise(n=n())
 
 usethis::use_data(lego_wrangle)
 
+
+#Ben's data
 alpha3codes <- read.csv("alphacodes.csv")
 
 world_climate_data <- read.csv("world_climate_data.csv")
 
 world_climate <- world_climate %>%
-  select(1:4, x1960:x2020) %>%
-  pivot_longer(cols = starts_with("x"), names_to = "year", values_to = "measure") %>%
-  filter(!is.na(measure)) %>%
-  mutate(year = sub('x', '', year),
+  dplyr::select(1:4, x1960:x2020) %>%
+  tidyr::pivot_longer(cols = starts_with("x"), names_to = "year", values_to = "measure") %>%
+  dplyr::filter(!is.na(measure)) %>%
+  dplyr::mutate(year = sub('x', '', year),
          year = lubridate::ymd(year, truncated = 2L),
          measure = round(measure, 2))
 world_climate <- world_climate %>%
-  left_join(alpha3codes, by = c("country_code" = "alpha_3"))
+  dplyr::left_join(alpha3codes, by = c("country_code" = "alpha_3"))
+
+usethis::use_data(world_climate)
