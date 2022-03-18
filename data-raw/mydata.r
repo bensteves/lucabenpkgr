23007aab98d8a8b27bf062a54e35faccf190d071
Ben Steves <bjs48@students.calvin.edu>
2022-03-18 01:11:39 -0400

    ran mydata.R for rdas. added vignette w/out code

--- data-raw/mydata.r
+++ data-raw/mydata.r
@@ -1,31 +1,33 @@
 #Luca's data
-lego_sets <- read.csv("sets.csv")
 
-lego_themes <- read.csv("themes.csv")
+lego_sets <- read.csv("data-raw/sets.csv")
+
+lego_themes <- read.csv("data-raw/themes.csv")
 
 lego <- merge(lego_themes, lego_sets, by.x ="id", by.y ="theme_id", all.x = TRUE, all.y = FALSE)
 
-lego_wrangle <- lego %>%
-  dplyr::group_by(name.x, year) %>%
-  dplyr::filter(grepl('Star Wars', name.x)) %>%
-  dlpyr::summarise(n=n())
+lego_wrangle <- lego |>
+  dplyr::group_by(name.x, year) |>
+  dplyr::filter(grepl('Star Wars', name.x)) |>
+  dplyr::summarise(n=dplyr::n())
 
 usethis::use_data(lego_wrangle)
 
 
 #Ben's data
-alpha3codes <- read.csv("alphacodes.csv")
+alpha3codes <- read.csv("data-raw/alphacodes.csv") |> janitor::clean_names()
 
-world_climate_data <- read.csv("world_climate_data.csv")
+world_climate_data <- read.csv("data-raw/world_climate_data.csv") |> janitor::clean_names()
 
-world_climate <- world_climate %>%
-  dplyr::select(1:4, x1960:x2020) %>%
-  tidyr::pivot_longer(cols = starts_with("x"), names_to = "year", values_to = "measure") %>%
-  dplyr::filter(!is.na(measure)) %>%
+world_climate <- world_climate_data |>
+  dplyr::select(1:4, x1960:x2020) |>
+  tidyr::pivot_longer(cols = starts_with("x"), names_to = "year", values_to = "measure") |>
+  dplyr::filter(!is.na(measure)) |>
   dplyr::mutate(year = sub('x', '', year),
          year = lubridate::ymd(year, truncated = 2L),
          measure = round(measure, 2))
-world_climate <- world_climate %>%
+
+world_climate <- world_climate |>
   dplyr::left_join(alpha3codes, by = c("country_code" = "alpha_3"))
 
 usethis::use_data(world_climate)
