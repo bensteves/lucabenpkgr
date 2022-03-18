8c603fedf6f7691a49ecde4896df8e720df583d4
Ben Steves <bjs48@students.calvin.edu>
2022-03-18 02:36:52 -0400

    documentation

--- R/df_apply.R
+++ R/df_apply.R
@@ -1,3 +1,15 @@
+#' Apply a function on each column of a data frame given a condition
+#'
+#' @param .data A data frame
+#' @param .f A function to apply on the data frame
+#' @param .condition Default value "is.numeric", but can be another data type
+#' @param .else Default value "identity" (skips over a column)
+#' @param ... 
+#'
+#' @return A tibble
+#' @export
+#'
+#' @examples
 df_apply <- function(.data, .f, .condition = is.numeric, .else = identity, ...) {
   .data |> lapply(
     function(x) 
