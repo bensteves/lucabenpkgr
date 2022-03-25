a38488f1044c25d427c6d9cb27dfef33df5ca5ed
Ben Steves <bjs48@students.calvin.edu>
2022-03-25 12:24:30 -0400

    testing functions

--- tests/testthat/test-df_apply.R
+++ tests/testthat/test-df_apply.R
@@ -1,8 +1,20 @@
-test_that("multiplication works", {
-  expect_equal(df_apply(lego_wrangle, keep, is.factor,  ), 4)
+require(purrr)
+
+#returns no rows
+factor_keep <- lego_wrangle %>%
+  keep(is.factor)
+
+#returns 2 rows
+numeric.keep <- lego_wrangle %>%
+  keep(is.numeric)
+
+test_that("df_apply - keep(factor)", {
+  expect_equal(df_apply(lego_wrangle, keep, is.factor), factor_keep)
 })
 
-
+test_that("df_apply - keep(numeric)", {
+  expect_equal(df_apply(lego_wrangle, keep, is.factor), numeric_keep)
+})
 
 df_apply <- function(.data, .f, .condition = is.numeric, .else = identity, ...) {
   .data |> lapply(
