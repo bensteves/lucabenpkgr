5ed71c5c0a2a75807c0711b74aaf5af51588afc4
Ben Steves <bjs48@students.calvin.edu>
2022-03-25 22:34:28 -0400

    added summary stat

--- tests/testthat/test-df_apply.R
+++ tests/testthat/test-df_apply.R
@@ -4,9 +4,11 @@ require(magrittr)
 require(tibble)
 require(lubridate)
 
-#ROUND
+
+#ROUND()
 digits = 2
 
+#trivial to round year, but it is listed as an int in Luca's training set
 rounded_lego <-lego_wrangle |>
   mutate(n = round(n, digits),
          year = round(year, digits)) |>
@@ -24,7 +26,7 @@ test_that("df_apply - round() - climate", {
   expect_equal(df_apply(world_climate, round, is.numeric, digits = digits), rounded_climate)
 })
 
-#To Upper
+#TOUPPER()
 upper_lego <- lego_wrangle |>
   mutate(name.x = toupper(name.x)) |>
   as_tibble()
@@ -41,11 +43,20 @@ test_that("df_apply - toupper() - climate", {
   expect_equal(df_apply(world_climate, toupper, is.character), upper_climate)
 })
 
-#Change dates to just year
+#YEAR() (extracts only the year)
 today_climate <- world_climate |>
   mutate(year = year(year)) |> 
   as_tibble()
 
 test_that("df_apply - year() - climate", {
   expect_equal(df_apply(world_climate, year, is.Date), today_climate)
+})
+
+#MEAN()
+mean_climate <- world_climate |>
+  mutate(across(where(is.numeric), mean, digits = digits)) |>
+  as_tibble()
+
+test_that("df_apply - mean - climate", {
+  expect_equal(df_apply(world_climate, mean, is.numeric), mean_climate)
 })
